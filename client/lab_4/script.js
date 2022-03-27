40dc2d2a3f0134985ab417b9f3ca2d4ae3ac3862
RandallMentzos <theguy77@umd.edu>
2022-03-27 08:35:55 -0400

    script added

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,5 +1,48 @@
-async function mainEvent () {
-  console.log('DOM Content Loaded.');
+let currentImg;
+let imgs;
+let numImgs;
+
+function updateVisible() {
+  console.log('reached img update function');
+  console.log(imgs);
+  for (const image of imgs) {
+    image.classList.remove('visible');
+    image.classList.add('hidden');
+  };
+  imgs[currentImg].classList.remove('visible');
+  imgs[currentImg].classList.add('visible');
+};
+  
+function slideDown() {
+  if (currentImg === 0) {
+    currentImg = numImgs - 1;
+  } else {
+    currentImg -= 1;
+  }
+  updateVisible();
 }
 
-document.addEventListener('DOMContentLoaded', async () => mainEvent());
+function slideUp() {
+  if (currentImg === numImgs - 1) {
+    currentImg = 0;
+  } else {
+    currentImg += 1;
+  }
+  updateVisible();
+}
+
+function mainEvent() {
+  console.log('DOM Content Loaded.');
+
+  currentImg = 0;
+  imgs = document.querySelectorAll('.carousel-item');
+  numImgs = imgs.length;
+
+  previous = document.querySelector('#prev');
+  next = document.querySelector('#next');
+
+  previous.addEventListener('click', () => slideDown());
+  next.addEventListener('click', () => slideUp());
+}
+
+document.addEventListener('DOMContentLoaded', () => mainEvent());
