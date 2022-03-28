05c1435ec5756fe32b4fabf3f776bb84f749e0de
RandallMentzos <theguy77@umd.edu>
2022-03-27 23:14:11 -0400

    random int, init resto list, click event & hard link

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,6 +1,15 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); 
+};
+
 function dataHandler (restoArray) {
   console.log('fired data handler');
-  console.table(restoArray); // this is called "dot notation"
+  console.table(restoArray); 
+  const range = [...Array(15).keys()];
+  
+  // this is called "dot notation"
   // arrayFromJson.data - we're accessing a key called 'data' on the returned object
   // it contains all 1,000 records we need
 }
@@ -8,18 +17,17 @@ function dataHandler (restoArray) {
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form');
   const button = document.querySelector('.submit');
-  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+  button.style.display = 'none';
+  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
 
-  console.log(arrayfromJson);
-
-  button.style.display = 'none';
-
-  if (arrayFromJson.data.length > 0) {
-    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-      submitEvent.preventDefault(); // This prevents your page from refreshing!
-      console.log('form submission'); // this is substituting for a "breakpoint";
-      dataHandler(arrayfromJson.data);
+  if (arrayFromJson.length > 0) {
+    button.style.display = 'block';
+    console.log('button printed');
+    button.addEventListener('click', async (submitEvent) => {
+      submitEvent.preventDefault();
+      console.log('clicked'); 
+      dataHandler(arrayFromJson);
     });
   }
 }
